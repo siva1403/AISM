4a9739676cc47bcce9c8b99d7fffa3dae9602244
Nagabharath1234 <bharathchowdhary79@gmail.com>
2026-05-07 22:43:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.config import get_settings
+from app.config import get_setting
 
 settings = get_settings()
 
